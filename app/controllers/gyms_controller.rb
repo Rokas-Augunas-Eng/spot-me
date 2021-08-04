58f3a8a9c0acf6b6f6d710a186415a1a74222748
rokas augunas <augunasr@gmail.com>
2021-08-04 21:14:30 +0100

    index and show page set up

--- app/controllers/gyms_controller.rb
+++ app/controllers/gyms_controller.rb
@@ -3,6 +3,13 @@ class GymsController < ApplicationController
 
   def index
     @gyms = Gym.all
+
+     @markers = @gyms.geocoded.map do |gym|
+      {
+        lat: gym.latitude,
+        lng: gym.longitude
+      }
+    end 
   end
 
   def new
@@ -20,6 +27,7 @@ class GymsController < ApplicationController
 
   def show
     @gym = Gym.find(params[:id])
+    @booking = Booking.new 
   end
 
   def edit
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Gym.destroy_all
+puts "Database deleted"
+
+10.times do 
+    Gym.create!([{
+      equipment: "Weights",
+      location: "Hoxton",
+      type_of_gym: "Lifting",
+      name: "Lifting gym for the Bros",
+      user_id: 2
+},
+{
+      equipment: "Running",
+      location: "London Bridge",
+      type_of_gym: "Cardio",
+      name: "Running away from your troubles",
+      user_id: 1
+}])
+end
+puts "Created #{Gym.count} gyms"
+

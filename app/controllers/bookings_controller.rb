f27179c4cd2d83e01f854523f4fd1e0d34b9d997
rokas augunas <augunasr@gmail.com>
2021-08-05 20:21:40 +0100

    demo preparation

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,16 +1,22 @@
 class BookingsController < ApplicationController
-  before_action :find_gym, only: [:new, :create, :index]
+  # before_action :find_gym, only: [:new, :create, :index]
 
+  def index
+    @bookings = Booking.all
+  end
+  
   def new
+  # @gym = Gym.find(params[:id])
   @booking = Booking.new 
   end
 
   def create
     @booking = Booking.new(bookings_params)
-    @gym = Gym.find(params[:gym_id])
+    @booking.user = current_user
+    find_gym
     @booking.gym = @gym
     if @booking.save
-    redirect_to gym_path(@gym)
+      redirect_to gym_path(@gym)
     else
       render :new  
     end 
@@ -23,13 +29,17 @@ class BookingsController < ApplicationController
     redirect_to gym_path(@gym)
   end
 
+  def show
+    @bookings = Booking.find(params[:id])
+  end
+
   private
 
   def find_gym
-    @gym = Gym.find(params[:id])
+    @gym = Gym.find(params[:gym_id])
   end
 
   def booking_params
-    params.require(:booking).permit(:booking_start, :booking_end)
+    params.require(:booking).permit(:booking_start, :booking_end, :user_id, :gym_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users 
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'profile', to: 'pages#profile'
+
   resources :gyms 
   resources :bookings, only:[:new, :create, :index]
 end
